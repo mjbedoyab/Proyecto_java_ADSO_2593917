20524573e6cd7dc09ce2406a48421911894be806
unknown <jadebb2012@gmail.com>
2023-11-10 07:06:30 -0500

    agricultura

---
+++ APIenPHP-agricultura/agricultor/getPersona.php
@@ -0,0 +1,28 @@
+<?php 
+	header("Access-Control-Allow-Origin: * "); // Permite el acceso desde cualquier origen, o usa "http://localhost" si solo quieres permitirlo desde localhost.
+	header("Access-Control-Allow-Methods: GET, POST");
+	header("Access-Control-Allow-Headers: Content-Type");
+
+    include 'Conexion.php';
+   
+    if (!empty($_GET['cedula']) || !empty($_GET['nombre']) ) {
+        $cedula = $_GET['cedula'];
+        $nombre = $_GET['nombre'];
+        
+	    $consulta = $base_de_datos->query("SELECT * FROM agricultores WHERE cedula = '$cedula' or nombre = '$nombre' ");
+	    $datos = $consulta->fetchAll();
+        $respuesta['registros'] = $datos;
+        echo json_encode($respuesta);
+	}else{
+        $respuesta = [
+                        'status' => false,
+                        'mesagge' => "ERROR##DATOS##GET",
+                        '$_GET' => $_GET,
+                        '$_POST' => $_POST
+                      ];
+        echo json_encode($respuesta);
+    }
+    
+?>
+
+
